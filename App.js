d166377861a9ddd8750792bced9bf5fc5a18793c
90dandan <1brokendrumhead@gmail.com>
2021-12-13 13:52:18 -0600

    api test

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
-const YourApp = () => {
+const getPopularMovies = async () => {
+  const resp = await axios.get('https://api.themoviedb.org/3/movie/550?api_key=ab1b7ed23a01c9bcca197c9c76f03757');
+  return(resp.data.results);
+};
+
+const App = () => {
+  const [movie, setMovie] = useState('');
+
+  useEffect(() => {
+  getPopularMovies().then(movies => {
+    setMovie(movies[0]);
+    });
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>
-        testing a push 
+         text{movie.original_title}
       </Text>
     </View>
   );
 }
 
-export default YourApp;
+export default App;
