9d08300f5e3b5bee468e85ce6879865a3158154d
90dandan <1brokendrumhead@gmail.com>
2021-12-15 13:45:21 -0600

    added home page images

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import { getPopularMovies } from './services/services';
+import React from 'react';
+import { View } from 'react-native';
+import Home from './screens/Home';
 
 const App = () => {
-  const [movie, setMovie] = useState('');
-
-  useEffect(() => {
-  getPopularMovies().then(movies => {
-    setMovie(movies[0]);
-    });
-  }, []);
-
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-         Title: {movie.original_title}
-      </Text>
+      <Home></Home>
     </View>
   );
 }
--- services/services.js
+++ services/services.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
-export const getPopularMovies = async () => {
-    const resp = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=ab1b7ed23a01c9bcca197c9c76f03757');
-    return(resp.data.results);
-  };
+const apiUrl = 'https://api.themoviedb.org/3/';
+const apiKey = 'api_key=ab1b7ed23a01c9bcca197c9c76f03757';
 
+export const getPopularMovies = async () => {
+    const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
+    return(resp.data.results);
+};
+
+export const getUpcomingMovies = async () => {
+    const resp = await axios.get(`${apiUrl}/movie/upcoming?${apiKey}`);
+    return(resp.data.results);
+};
+
+export const getPopularTv = async () => {
+    const resp = await axios.get(`${apiUrl}/tv/popular?${apiKey}`);
+    return(resp.data.results);
+};
